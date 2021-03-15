a1750d7b7a46d345e40352d21367fbfd52915fc5
alpha <nuruddinsayeed@gmail.com>
2021-03-15 15:04:26 +0600

    added searching to user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 
 from profiles_api import serializers
@@ -122,3 +123,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfele,)
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email',)
